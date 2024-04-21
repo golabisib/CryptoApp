b5629a0d60067d2245abf6481d73b5e52cb2ae32
Roozbeh Rigi Jangjo <silentroozbeh@gmail.com>
2024-04-21 12:31:46 +0330

    The Service folder is added to repo the api addreses. and secure the keyOfApi

--- src/components/templates/HomePage.jsx
+++ src/components/templates/HomePage.jsx
@@ -1,14 +1,18 @@
 import { useEffect, useState } from "react"
 import TableCoin from "./TableCoin"
+import { getCoinList } from "../../services/cryptoApi";
 
 function HomePage() {
     const [coins, setCoins] = useState([]);
 
     useEffect( () => {
-        fetch(
-            "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&per_page=20&x_cg_demo_api_key=CG-CwMqfWH5PZiNEFuY6Yxzyf5d"
-        ).then( (res) => res.json())
-         .then( (json) => setCoins(json))
+        const getData = async () => {
+            const res = await fetch(getCoinList());
+            const json = await res.json();
+            setCoins(json);
+        };
+
+        getData();
     }, []);
 
   return (
