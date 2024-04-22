8776738b857432477ed8f03069cf248e15d3827a
Roozbeh Rigi Jangjo <silentroozbeh@gmail.com>
2024-04-22 21:47:36 +0330

    The styles of table is added

--- src/components/templates/TableCoin.jsx
+++ src/components/templates/TableCoin.jsx
@@ -5,12 +5,14 @@ import { FidgetSpinner } from "react-loader-spinner"
 import chartUp from "../../assets/chart-up.svg"
 import chartDown from "../../assets/chart-down.svg"
 
+import styles from "../modules/TableCoin.module.css"
+
 function TableCoin({coins, isLoading}) {
     console.log(coins)
   return (
-    <div>
+    <div className={styles.container}>
         {isLoading ? <FidgetSpinner /> :
-        <table>
+        <table className={styles.table}>
         <thead>
             <tr>
                 <th>Coin</th>
@@ -56,14 +58,16 @@ const TableRow = ({
     return (
         <tr>
                         <td>
-                            <div>
+                            <div className={styles.symbol}>
                                 <img src={image} alt="Coin Image" />
                                 <span>{symbol.toUpperCase()}</span>
                             </div>
                         </td>
                         <td>{name}</td>
                         <td>${current_price.toLocaleString()}</td>
-                        <td>{price_change.toFixed(2)}%</td>
+                        <td className={price_change > 0 ? styles.success : styles.error} >
+                            {price_change.toFixed(2)}%
+                        </td>
                         <td>{total_volume.toLocaleString()}</td>
                         <td><img src={price_change > 0 ? chartUp : chartDown} alt="Chart Condition" /></td>
                     </tr>
