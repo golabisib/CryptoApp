229ffe736ded944b3a3b80ea2cf0fd3261cefb75
Roozbeh Rigi Jangjo <silentroozbeh@gmail.com>
2024-04-22 22:50:39 +0330

    The paginate part is added

---
+++ src/components/modules/Pagination.jsx
@@ -0,0 +1,41 @@
+import PropTypes from "prop-types"
+
+
+function Pagination({page, setPage}) {
+
+    const previousHandler = () => {
+        if (page <= 1) return;
+        setPage( (page) => page - 1);
+    }
+
+    const nextHandler = () => {
+        if (page >= 10) return;
+        setPage( (page) => page + 1)
+    }
+  return (
+    <div>
+        <button onClick={previousHandler}>previous</button>
+        <p style={{ color: page === 1 ? "red" : "inherit"}}>1</p>
+        <p style={{ color: page === 2 ? "red" : "inherit"}}>2</p>
+        {
+            page > 2 && page < 9 && (
+                <>
+                <span>...</span>
+                <p style={{ color: page === page ? "red" : "inherit"}}>{page}</p>
+                </>
+            )
+        }
+        <span>...</span>
+        <p style={{ color: page === 9 ? "red" : "inherit"}}>9</p>
+        <p style={{ color: page === 10 ? "red" : "inherit"}}>10</p>
+        <button onClick={nextHandler}>next</button>
+    </div>
+  )
+}
+
+Pagination.propTypes = {
+  page: PropTypes.number,
+  setPage: PropTypes.func
+}
+
+export default Pagination
--- src/components/templates/HomePage.jsx
+++ src/components/templates/HomePage.jsx
@@ -1,25 +1,29 @@
 import { useEffect, useState } from "react"
 import TableCoin from "./TableCoin"
 import { getCoinList } from "../../services/cryptoApi";
+import Pagination from "../modules/Pagination";
 
 function HomePage() {
     const [coins, setCoins] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
+    const [page, setPage] = useState(1);
+
 
     useEffect( () => {
+        setIsLoading(true);
         const getData = async () => {
-            const res = await fetch(getCoinList());
+            const res = await fetch(getCoinList(page));
             const json = await res.json();
             setCoins(json);
             setIsLoading(false);
         };
 
         getData();
-    }, []);
+    }, [page]);
 
   return (
     <div>
-        <h1>homepage</h1>
+        <Pagination page={page} setPage={setPage} />
         <TableCoin coins={coins} isLoading={isLoading} />
     </div>
   )
