9b88f5e619779a8b05e5e9d22878301f24984ef4
Roozbeh Rigi Jangjo <silentroozbeh@gmail.com>
2024-04-22 13:57:39 +0330

    The loading part is added

--- src/components/templates/TableCoin.jsx
+++ src/components/templates/TableCoin.jsx
@@ -1,39 +1,45 @@
+import PropTypes from "prop-types"
+import { FidgetSpinner } from "react-loader-spinner"
+
+
 import chartUp from "../../assets/chart-up.svg"
 import chartDown from "../../assets/chart-down.svg"
 
-import PropTypes from "prop-types"
-
-function TableCoin({coins}) {
+function TableCoin({coins, isLoading}) {
     console.log(coins)
   return (
     <div>
+        {isLoading ? <FidgetSpinner /> :
         <table>
-            <thead>
-                <tr>
-                    <th>Coin</th>
-                    <th>Name</th>
-                    <th>Price</th>
-                    <th>24h</th>
-                    <th>Total volume</th>
-                    <th></th>
-                </tr>
-            </thead>
-            <tbody>
-                {coins.map( (coin) => (
-                    <TableRow coin={coin} key={coin.id} />
-                ))}
-            </tbody>
-        </table>
+        <thead>
+            <tr>
+                <th>Coin</th>
+                <th>Name</th>
+                <th>Price</th>
+                <th>24h</th>
+                <th>Total volume</th>
+                <th></th>
+            </tr>
+        </thead>
+        <tbody>
+            {coins.map( (coin) => (
+                <TableRow coin={coin} key={coin.id} />
+            ))}
+        </tbody>
+    </table>
+        }
     </div>
   )
 }
-//`126
+
 TableCoin.propTypes = {
-  coins: PropTypes.array.isRequired
+  coins: PropTypes.array.isRequired,
+  isLoading: PropTypes.any
 }
 
 export default TableCoin
 
+//128
 
 //This component used only in here so we define in parent component
 
@@ -51,7 +57,7 @@ const TableRow = ({
         <tr>
                         <td>
                             <div>
-                                <img src={image} alt="" />
+                                <img src={image} alt="Coin Image" />
                                 <span>{symbol.toUpperCase()}</span>
                             </div>
                         </td>
@@ -59,7 +65,7 @@ const TableRow = ({
                         <td>${current_price.toLocaleString()}</td>
                         <td>{price_change.toFixed(2)}%</td>
                         <td>{total_volume.toLocaleString()}</td>
-                        <td><img src={price_change > 0 ? chartUp : chartDown} alt="" /></td>
+                        <td><img src={price_change > 0 ? chartUp : chartDown} alt="Chart Condition" /></td>
                     </tr>
     )
 }
